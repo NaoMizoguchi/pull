28387edce9344ac484f3a28d048ee094bdb5440e
NaoMizoguchi <N.MIZOGUCHI@BIT-WARE.CO.JP>
2025-05-09 10:08:14 +0900

    Update sort.js

--- sort.js
+++ sort.js
@@ -2,7 +2,7 @@ $ git checkout -b add-sort-func
 
 var sortNumber = function (number) {
    number.sort(function (a, b) {
-       if (a == b) {
+       if (a === b) {
            return 0;
        }
        return a < b ? -1 : 1;
